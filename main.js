2e9c5ee1f3e3c9d266b96c2f735f592dcd8cf8bd
muamarzidan <zidanprofix93@gmail.com>
2022-08-02 22:11:01 +0700

    add fun to image

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const searchButton = document.querySelector('#button-addon2');
 const inputKeyword = document.querySelector('.input-keyword');
+const backgroundImageee = document.body.style.backgroundImage = 'url(img/.jpg)'
 document.body.style.backgroundImage = 'url(img/mendung.jpg)'
 
 searchButton.addEventListener('click', function() {
@@ -34,6 +35,14 @@ searchButton.addEventListener('click', function() {
                 <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
                 <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>
                 <img src=\"img/rain.svg" width=\"400px\" height=\"150px\">`
+            } else {
+                result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
+                <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
+                <p style="margin-bottom: 17px;">Temperature from ${response.main.temp_min}°С to ${response.main.temp_max}°С</p>
+                <h5>Wind Speed : ${response.wind.speed} m/s</h5>
+                <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
+                <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>
+                <img src=\"img/storm.svg" width=\"400px\" height=\"150px\">`
             }
         });
     inputKeyword.value = null;
