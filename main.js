bce4098115a374396aed8449a471bf2f4fbc21fa
muamarzidan <zidanprofix93@gmail.com>
2022-08-03 10:18:47 +0700

    add fun to set backgrounf

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 const searchButton = document.querySelector('#button-addon2');
 const inputKeyword = document.querySelector('.input-keyword');
 // const backgroundImagerain = document.body.style.backgroundImage = 'url(img/rain.gif)';
-document.body.style.backgroundImage = 'url(img/mendung.jpg)';
+// document.body.style.backgroundImage = 'url(img/mendung.jpg)';
 
 searchButton.addEventListener('click', function() {
-    // function backgroundImage() {
-    //     if (inputKeyword.value.response.main.temp >= 30) {
-    //         return document.body.style.backgroundImage = 'url(img/cleargif.gif)';
-    //     } else if (inputKeyword.value.response.main.temp <= 25) {
-    //         return document.body.style.backgroundImage = 'url(img/raingif.gif)';
-    //     }
-    // }
+    function backgroundImage() {
+        if (inputKeyword.value.response.main.temp >= 30) {
+            return document.body.style.backgroundImage = 'url(img/cleargif.gif)';
+        } else if (inputKeyword.value.response.main.temp <= 25) {
+            return document.body.style.backgroundImage = 'url(img/raingif.gif)';
+        }
+    }
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ inputKeyword.value +"&appid=6f0b1403f99965066346e9142ce20053&units=metric")
         .then(response => response.json())
         .then(response => {
             let result = document.querySelector('.result')
             console.log(result);
+
             const suhu = response.main.temp;
+            const backgroundImage = document.body.style.backgroundImage;
+            if (suhu >= 30.00 && suhu <= 40.00 ) {
+                document.body.style.backgroundImage = 'url(img/cleargif.gif)';
+            } else if (suhu >= 25.00 && suhu <= 30.00) {
+                document.body.style.backgroundImage = 'url(img/raingif.gif)';
+            }
+
+            
             if(suhu >= 30.00 && suhu <= 40.00) {
                 result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
                 <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
@@ -25,7 +34,8 @@ searchButton.addEventListener('click', function() {
                 <h5>Wind Speed : ${response.wind.speed} m/s</h5>
                 <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
                 <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>
-                <img src=\"img/storm.svg" width=\"400px\" height=\"150px\">`
+                <img src=\"img/clear.svg" width=\"400px\" height=\"150px\">`
+                
             } else if (suhu >= 25.00 && suhu <= 30.00) {
                 result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
                 <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
@@ -33,7 +43,7 @@ searchButton.addEventListener('click', function() {
                 <h5>Wind Speed : ${response.wind.speed} m/s</h5>
                 <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
                 <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>
-                <img src=\"img/storm.svg" width=\"400px\" height=\"150px\">`
+                <img src=\"img/cloud.svg" width=\"400px\" height=\"150px\">`
             } else if (suhu >= 20.00 && suhu <= 25.00) {
                 result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
                 <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
@@ -41,7 +51,7 @@ searchButton.addEventListener('click', function() {
                 <h5>Wind Speed : ${response.wind.speed} m/s</h5>
                 <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
                 <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>
-                <img src=\"img/storm.svg" width=\"400px\" height=\"150px\">`
+                <img src=\"img/rain.svg" width=\"400px\" height=\"150px\">`
             } else {
                 result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
                 <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
